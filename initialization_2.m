6a42475ed0849a59e7d27f9bab24b3f8c26d2ba8
Dismac <93903389+Dismac@users.noreply.github.com>
2022-12-03 14:56:40 +0800

    Add files via upload

--- initialization_2.m
+++ initialization_2.m
@@ -13,31 +13,33 @@ for i = 1:num_file
     epsilon(i) = 0.016*i;
 end
 Cmax=19;%最多缓存数
-loop1max=10;
-loop2max=10;
-allalpha=zeros(loop1max,K);
-allreward=zeros(K,1);
+loop1max=10;   %生成用户请求和缓存循环数
+loop2max=10;   %生成功率分配向量循环数
+allalpha=zeros(loop1max,K);  %最终被选择的最佳的功率分配向量α
+allreward=zeros(K,1);        %最终各loop1的成功接收用户数reward
 
 for loop1=1:loop1max
-    Cnum=randi(Cmax,[1,4]);
-    CCij=zeros(K,num_file);
+    Cnum=randi(Cmax,[1,K]);    %生成用户缓存文件数
+    CCij=zeros(K,num_file);     %CCij是用户缓存表
 
+    %随机生成用户缓存
  for i=1:K
     Ctemp=randperm(38);
-    Cpositon=Ctemp(1:Cnum(i));
+    Cpositon=Ctemp(1:Cnum(i));   %用randperm防止重复，生成具体用户缓存文件
      for j=1:Cnum(i)
         CCij(i,Cpositon(j))=1;
      end
  end
+
  ftemp=randperm(38);
- fask=ftemp(1:4);
- Cij=zeros(K,K);
+ fask=ftemp(1:4);    %生成用户请求文件，这里是假设用户请求文件不同
+ Cij=zeros(K,K);     %论文中的用户i是否缓存文件j
  for i=1:K
      for j=1:K
          Cij(i,j)=CCij(i,fask(j));
      end
  end
- Sij=zeros(K,K);
+ Sij=zeros(K,K);    %论文中的用户优势
  for i=1:K
      for j=1:K
          if Cij(i,j)==0 
@@ -49,8 +51,10 @@ for loop1=1:loop1max
      end
  end
  Sij=Sij.';
- Sij=reshape(Sij,1,K*K);
+ Sij=reshape(Sij,1,K*K);   %方便后续计算reward
 
+
+ %这里计算框架是根据论文中的伪代码2
  for loop2=1:loop2max
      alpha=rand(1,K);
   for k=1:K
@@ -68,7 +72,7 @@ for loop1=1:loop1max
           end
       end
   end
-  success=ones(1,K);
+  success=ones(1,K);  %各用户是否成功接收
   for i=1:K
       for j=1:K
           sumtemp=0;
@@ -84,7 +88,7 @@ for loop1=1:loop1max
   reward=sum(success);
   allreward(loop2)=reward;
  end
-  alpha(loop1,:)=alpha;
+  allalpha(loop1,:)=alpha;
 end
 
 

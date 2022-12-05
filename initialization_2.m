f0e0a6b86fe3c824f9968613d27d21034a01ef3a
Dismac <93903389+Dismac@users.noreply.github.com>
2022-12-05 18:02:57 +0800

    Add files via upload

--- initialization_2.m
+++ initialization_2.m
@@ -4,8 +4,6 @@ d_i = 1; % i=1,2,...,K
 sigma2 = 1; %信道高斯噪声方差
 lambda_1=1;
 lambda_2=1;
-H1 = random(makedist('Exponential',lambda_1)); %|h1|^2，满足指数分布
-H2 = random(makedist('Exponential',lambda_2)); %|h1|^2，满足指数分布
 num_file = 38; %文件数目
 epsilon = zeros(0,num_file);
 betai=ones(1,K);
@@ -14,10 +12,12 @@ for i = 1:num_file
 end
 Cmax=19;%最多缓存数
 loop1max=10;   %生成用户请求和缓存循环数
+Amax=10;
 loop2max=10;   %生成功率分配向量循环数
 allalpha=zeros(loop1max,K);  %最终被选择的最佳的功率分配向量α
+avereward=zeros(K,1);
 allreward=zeros(K,1);        %最终各loop1的成功接收用户数reward
-
+average_reward=0;
 for loop1=1:loop1max
     Cnum=randi(Cmax,[1,K]);    %生成用户缓存文件数
     CCij=zeros(K,num_file);     %CCij是用户缓存表
@@ -55,13 +55,15 @@ for loop1=1:loop1max
 
 
  %这里计算框架是根据论文中的伪代码2
- for loop2=1:loop2max
-     alpha=rand(1,K);
+ for loop2=1:Amax
+  for loop3=1:loop2max
+      alpha=rand(1,K);
   for k=1:K
       if Sij(K*(k-1)+k)==0
           alpha(k)=0;
       end
   end
+  
   alphasum=sum(alpha);
   if alphasum==0
       alpha=1/K*ones(1,K);
@@ -90,10 +92,19 @@ for loop1=1:loop1max
       end
   end
   reward=sum(success);
-  allreward(loop2)=reward;
+  avereward(loop3)=reward;
+  end
+  allreward(loop2)=mean(avereward);
  end
   allalpha(loop1,:)=alpha;
 end
+
+
+
+
+
+
+
 
 
 
